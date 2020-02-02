cbb4c9a4f05bc49cda67fb333d5564947fc10ad6
gurkerl83 <markus_gritsch@gmx.de>
2020-02-02 12:53:19 +0100

    Fix ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,58 @@
-{
-  "extends": [
+module.exports = {
+  env: {
+    browser: true
+  },
+  parser: "@typescript-eslint/parser",
+  /*
+  parserOptions: {
+    project: [
+      path.resolve(__dirname, "./tsconfig.json"),
+      path.resolve(__dirname, "./webpack/tsconfig.json")
+    ]
+  },
+  */
+  plugins: ["@typescript-eslint"],
+  extends: [
     "airbnb-base",
     "plugin:@typescript-eslint/recommended",
     "prettier",
     "prettier/@typescript-eslint"
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": ["./tsconfig.json"]
-  },
-  "plugins": [],
-  "env": {
-    "browser": true
-  },
-  "settings": {
-    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+  settings: {
     "import/parsers": {
       "@typescript-eslint/parser": [".ts", ".tsx"]
     },
     "import/resolver": {
-      "node": {
-        "extensions": [".ts", ".tsx"]
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
       }
     }
   },
-  "rules": {
+  rules: {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        mjs: "never",
+        ts: "never",
+        tsx: "never"
+      }
+    ],
+    "import/prefer-default-export": "off",
     "import/no-unresolved": "off",
     "import/no-extraneous-dependencies": [
       "error",
       {
-        "devDependencies": ["webpack/*"]
+        devDependencies: ["webpack/*"]
       }
     ],
     "no-use-before-define": "off",
-    "import/prefer-default-export": "off",
     "lines-between-class-members": [
       "error",
       "always",
-      { "exceptAfterSingleLine": true }
+      { exceptAfterSingleLine: true }
     ],
     "no-return-assign": "error",
     "@typescript-eslint/no-empty-interface": "off",
@@ -46,14 +61,14 @@
     "@typescript-eslint/explicit-member-accessibility": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/array-type": ["error", { "default": "generic" }],
+    "@typescript-eslint/array-type": ["error", { default: "generic" }],
     "no-shadow": "off",
     "no-param-reassign": [
       "error",
       {
-        "props": true,
-        "ignorePropertyModificationsFor": ["draftState"]
+        props: true,
+        ignorePropertyModificationsFor: ["draftState"]
       }
     ]
   }
-}
+};
